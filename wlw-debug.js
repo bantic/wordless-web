b12ddf7ba8939128e4dc97cf1dfe39d6590c1ac9
Cory Forsyth <cory.forsyth@gmail.com>
2012-04-22 18:19:32 -0400

    added debug version

---
+++ wlw-debug.js
@@ -0,0 +1,36 @@
+;(function() {
+  alert('hello');
+  window.__wlw = {};
+  window.__wlw.loading_jQuery = false;
+  window.__wlw.loader = function() {
+    if (typeof(jQuery) === 'undefined') {
+      if (window.__wlw.loading_jQuery) {
+        alert('loading...trying again in 100ms');
+        setTimeout(window.__wlw.loader, 100);
+      } else {
+        alert('no jquery...loading for the first time');
+        window.__wlw.loading_jQuery= true;
+        var jq_script = document.createElement('script');
+        jq_script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+        document.body.appendChild(jq_script);
+        window.__wlw.loader();
+      }
+    } else {
+      alert('got JQUERY');
+      window.__wlw.hideWords(jQuery);
+    }
+  }
+  window.__wlw.hideWords = function(jQuery) {
+    alert("hiding words");
+    jQuery('body').find('*').each( function() {
+      $(this).css({'-webkit-transition':'all 1s ease-out',
+                   '-moz-transition':'all 1s ease-out',
+                   '-o-transition':'all 1s ease-out',
+                   'transition':'all 1s ease-out',
+                   'color':'#FFF',
+                   'background-color':'#FFF',
+                   'border-color':'#FFF'});
+    });
+  }
+  window.__wlw.loader();
+})();
