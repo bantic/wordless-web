d6ae895ac3a53126c6788e7311aa00fce343d540
Cory Forsyth <cory.forsyth@gmail.com>
2012-05-01 00:30:27 -0400

    few small fixes

--- javascripts/wlw-site.js
+++ javascripts/wlw-site.js
@@ -1,23 +1,36 @@
 $(document).ready(function(){
+  var bookmarklet_js = "javascript:void(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','//wordless-web-production.s3.amazonaws.com/wlw.js');document.body.appendChild(jsCode);}());";
+  $('#bookmarklet').attr('href', bookmarklet_js);
+  window.clicked_bookmarklet = false;
   $('#bookmarklet').click( function(e) {
-    if ($(this).attr('href') == '#') {
-      $('.inactive').removeClass('inactive');
-      var bookmarklet_js = "javascript:void(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','//wordless-web-production.s3.amazonaws.com/wlw.js');document.body.appendChild(jsCode);}());";
-      $(this).attr('href', bookmarklet_js);
-      $(this).attr('title','Wordless Web');
-      $('#about_link_outer').removeClass('hidden');
-      var offset = $('#bookmarklet').offset();
-      $('#arrow').removeClass('hidden').offset({
-        left: offset.left + 133,
-        top: offset.top - 234,
-      });
+    e.preventDefault();
+    if (window.clicked_bookmarklet) {
+      return;
     }
-    return false;
+    window.clicked_bookmarklet = true;
+    $('.inactive').removeClass('inactive');
+    var bookmarklet_js = "javascript:void(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','//wordless-web-production.s3.amazonaws.com/wlw.js');document.body.appendChild(jsCode);}());";
+    $(this).attr('href', bookmarklet_js);
+    $(this).attr('title','Wordless Web');
+    $('#about_link_outer').removeClass('hidden');
+    $('#arrow').removeClass('hidden');
+    /* this puts the arrow right next to the button
+    var offset = $('#bookmarklet').offset();
+    $('#arrow').removeClass('hidden').offset({
+      left: offset.left + 133,
+      top: offset.top - 234,
+    });
+   */
+  });
+  $('#header').click( function() {
+    $('#instructions').removeClass('hidden');
+    $('#about').addClass('hidden');
+    $('#about_link_outer').removeClass('hidden');
   });
   $('#about_link').click( function() {
     $('#instructions').addClass('hidden');
     $('#about').removeClass('hidden');
-    $('#about_link_outer').fadeOut();
+    $('#about_link_outer').addClass('hidden');
     return false;
   });
 });
