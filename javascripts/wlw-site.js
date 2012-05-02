4384d8f348e4c81b852ae9de83fbb9517dccea66
Cory Forsyth <cory.forsyth@gmail.com>
2012-05-01 22:50:41 -0400

    fix some bugs ji reported

--- javascripts/wlw-site.js
+++ javascripts/wlw-site.js
@@ -8,29 +8,31 @@ $(document).ready(function(){
       return;
     }
     window.clicked_bookmarklet = true;
-    $('.inactive').removeClass('inactive');
-    var bookmarklet_js = "javascript:void(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','//wordless-web-production.s3.amazonaws.com/wlw.js');document.body.appendChild(jsCode);}());";
-    $(this).attr('href', bookmarklet_js);
-    $(this).attr('title','Wordless Web');
-    $('#about_link_outer').removeClass('hidden');
+    $('#instructions').removeClass('hidden');
+    $('#about_link_wrapper').removeClass('hidden');
+    $('#header').removeClass('inactive');
     $('#arrow').removeClass('hidden');
-    /* this puts the arrow right next to the button
-    var offset = $('#bookmarklet').offset();
-    $('#arrow').removeClass('hidden').offset({
-      left: offset.left + 133,
-      top: offset.top - 234,
-    });
-   */
+    placeArrow();
   });
   $('#header').click( function() {
+    if (!window.clicked_bookmarklet) {
+      return false;
+    }
     $('#instructions').removeClass('hidden');
     $('#about').addClass('hidden');
-    $('#about_link_outer').removeClass('hidden');
   });
   $('#about_link').click( function() {
     $('#instructions').addClass('hidden');
     $('#about').removeClass('hidden');
-    $('#about_link_outer').addClass('hidden');
     return false;
   });
+  function placeArrow() {
+    var offset = $('#bookmarklet').offset();
+    $('#arrow').offset({
+      left: offset.left + 149,
+      top: offset.top - 232,
+    });
+  }
+  $(window).resize(placeArrow);
 });
+
