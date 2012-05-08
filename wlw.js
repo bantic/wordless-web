519cb9c3ef58e1cd7ea1176f8b8f5aae43ff9c7f
Cory Forsyth <cory.forsyth@gmail.com>
2012-05-08 19:43:22 -0400

    update wlw.js

--- wlw.js
+++ wlw.js
@@ -17,103 +17,9 @@
         window.__wlw.loader();
       }
     } else {
-      window.__wlw.fixCSSStyleDeclaration();
-      jQuery = window.__wlw.addWhitenToJquery(jQuery);
-      jQuery = window.__wlw.addStyleToJquery(jQuery);
       window.__wlw.hideWords(jQuery, true);
       window.__wlw.live(jQuery);
     }
-  }
-
-  window.__wlw.fixCSSStyleDeclaration = function() {
-    // see http://stackoverflow.com/questions/2655925/jquery-css-applying-important-styles
-    // For those who need them (< IE 9), add support for CSS functions
-    var isStyleFuncSupported = CSSStyleDeclaration.prototype.getPropertyValue != null;
-    if (!isStyleFuncSupported) {
-        CSSStyleDeclaration.prototype.getPropertyValue = function(a) {
-            return this.getAttribute(a);
-        };
-        CSSStyleDeclaration.prototype.setProperty = function(styleName, value, priority) {
-            this.setAttribute(styleName,value);
-            var priority = typeof priority != 'undefined' ? priority : '';
-            if (priority != '') {
-                // Add priority manually
-                var rule = new RegExp(RegExp.escape(styleName) + '\\s*:\\s*' + RegExp.escape(value) + '(\\s*;)?', 'gmi');
-                this.cssText = this.cssText.replace(rule, styleName + ': ' + value + ' !' + priority + ';');
-            } 
-        }
-        CSSStyleDeclaration.prototype.removeProperty = function(a) {
-            return this.removeAttribute(a);
-        }
-        CSSStyleDeclaration.prototype.getPropertyPriority = function(styleName) {
-            var rule = new RegExp(window.__wlw.escape(styleName) + '\\s*:\\s*[^\\s]*\\s*!important(\\s*;)?', 'gmi');
-            return rule.test(this.cssText) ? 'important' : '';
-        }
-    }
-  }
-
-  // Escape regex chars with \
-  window.__wlw.escape = function(text) {
-      return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-  }
-
-  window.__wlw.addStyleToJquery = function(jQuery) {
-    // The style function
-    jQuery.fn.style = function(styleName, value, priority) {
-        // DOM node
-        var node = this.get(0);
-        // Ensure we have a DOM node 
-        if (typeof node == 'undefined') {
-            return;
-        }
-        // CSSStyleDeclaration
-        var style = this.get(0).style;
-        // Getter/Setter
-        if (typeof styleName != 'undefined') {
-            if (typeof value != 'undefined') {
-                // Set style property
-                var priority = typeof priority != 'undefined' ? priority : '';
-                style.setProperty(styleName, value, priority);
-            } else {
-                // Get style property
-                return style.getPropertyValue(styleName);
-            }
-        } else {
-            // Get CSSStyleDeclaration
-            return style;
-        }
-    }
-    return jQuery;
-  }
-
-  window.__wlw.addWhitenToJquery = function(jQuery) {
-    jQuery.fn.whiten = function(include_transitions) {
-      var css_changes = {
-        '-webkit-transition':'all 1s ease-out',
-        '-moz-transition':'all 1s ease-out',
-        '-o-transition':'all 1s ease-out',
-        'transition':'all 1s ease-out'
-      };
-      var style_changes = {
-        'color':'#FFF',
-        'background-color':'#FFF',
-        'border-color':'#FFF',
-        'text-shadow':'none'
-      };
-      if (include_transitions) {
-        this.css(css_changes);
-      }
-      for (var style_change in style_changes) {
-        if (style_changes.hasOwnProperty(style_change)) {
-          this.style(style_change, style_changes[style_change], 'important');
-        }
-      }
-      // hide text filled into inputs
-      this.val('');
-      this.attr('placeholder','');
-      return this;
-    }
-    return jQuery;
   }
 
   window.__wlw.live = function(jQuery) {
@@ -129,14 +35,14 @@
     });
   };
 
-  window.__wlw.getTextNodesIn = function(el) {
+  window.__wlw.getTextNodesIn = function(jQuery, el) {
     return jQuery(el).find(":not(iframe)").andSelf().contents().filter(function() {
         return this.nodeType == 3;
     });
   };
 
   window.__wlw.hideWords = function(jQuery, include_transitions) {
-    window.__wlw.getTextNodesIn(jQuery('body')).remove();
+    window.__wlw.getTextNodesIn(jQuery, jQuery('body')).remove();
     jQuery('iframe').css('opacity',0);
     jQuery('input').val('').attr('placeholder','');
     jQuery('textarea').val('').attr('placeholder','');
